43a8fde0def812cb3fda7701f63908d07d08e689
vgtxc <vnkgtxc@163.com>
2024-04-22 10:43:23 +0800

    2024.04.22-10:43

--- tmp/helloWord.c
+++ tmp/helloWord.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
-
+#include<stdlib.h>
 int main(int argc,char* argv[],char* argenv[]){
 	printf("hello word\n");
 	for(int i=0;i<argc;i++){
 		printf("argv[%d]%s\n",i,argv[i]);
 	}
+	system("pause");
 	return 0;
 }
